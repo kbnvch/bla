4ecdf9f52a0273991b59b071587a09d2f12b80f5
kbnvch <77291360+kbnvch@users.noreply.github.com>
2023-01-25 13:50:28 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,14 @@
-<script>
-
-  alert("LABA DIENA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-
-</script>
+var config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      scene: {
+        preload: preload,
+        create: create,
+        update: update,
+      },
+    };
+  var game = new Phaser.Game(config);
+  function preload() {}
+  function create() {}
+  function update() {}
